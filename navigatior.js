2558c66b43469fe347aadb7a42d9cc8b7c5ff6ea
linhey <158179948@qq.com>
2018-04-10 15:00:32 +0800

    update

--- navigatior.js
+++ navigatior.js
@@ -12,10 +12,28 @@ nav.prototype.post_for_1 = function (name,body) {
 
 //回退至指定app页面
 nav.prototype.pop = function (value) {
+    function special(index) {
+        if (native_config.source == 0) {
+            this.post_for_1('execute','sp://tabbar/selectIndex?index=' + index);
+            var data = {}
+            data.vcName = 'tabbar';
+            this.post_for_1('pop',data)
+        } else {
+            this.post_for_1('push','sp:/myProfile/myProfile')
+        }
+    }
+
     // 1.x 兼容代码
     if (!(native_config.wp)) { return }
     if (!(native_config.wp > 2000)){
-       this.post_for_1("pop",value)
+        switch (value) {
+            case 'home': special(0);
+            case 'cateory': special(1);
+            case 'message': special(2);
+            case 'shopcart': special(3);
+            case 'myprofile': special(4);
+            default: this.post_for_1("pop",value);
+        }
         return
     }
 
