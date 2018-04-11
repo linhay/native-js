fad56e9eec051f884fe72fb6ece151dc56c0ccbf
linhey <158179948@qq.com>
2018-04-11 19:00:03 +0800

    update

--- storage.js
+++ storage.js
@@ -1,53 +1,6 @@
 var storage = function () {
 };
 
-var StorageEvent = {
-
-    _listeners: {},
-
-    addEvent: function (type, fn) {
-        if (typeof this._listeners[type] === "undefined") {
-            this._listeners[type] = [];
-        }
-        if (typeof fn === "function") {
-            this._listeners[type].push(fn);
-        }
-
-        return this;
-    },
-
-    fireEvent: function (type, param) {
-        var arrayEvent = this._listeners[type];
-        if (arrayEvent instanceof Array) {
-            for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
-                if (typeof arrayEvent[i] === "function") {
-                    arrayEvent[i](param);
-                }
-            }
-        }
-
-        return this;
-    },
-
-    removeEvent: function (type, fn) {
-        var arrayEvent = this._listeners[type];
-        if (typeof type === "string" && arrayEvent instanceof Array) {
-            if (typeof fn === "function") {
-                for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
-                    if (arrayEvent[i] === fn) {
-                        this._listeners[type].splice(i, 1);
-                        break;
-                    }
-                }
-            } else {
-                delete this._listeners[type];
-            }
-        }
-
-        return this;
-    }
-};
-
 var _cache_result_req_queue = [];
 var _cache_result_back_queue = {};
 
@@ -57,7 +10,7 @@ function cacheResult(name, value) {
         if (_cache_result_back_queue[_cache_result_req_queue[name]] === undefined) return;
     }
     var name = JSON.stringify(_cache_result_req_queue);
-    StorageEvent.fireEvent(name, _cache_result_back_queue);
+    NativeEvent.fireEvent(name, _cache_result_back_queue);
 }
 
 storage.prototype.app_cache = function (type, name, value) {
@@ -87,8 +40,8 @@ storage.prototype.get = function (list, cb) {
             this.app_cache('get', list[item]);
         }
 
-        StorageEvent.addEvent(name, function (value) {
-            StorageEvent.removeEvent(name);
+        NativeEvent.addEvent(name, function (value) {
+            NativeEvent.removeEvent(name);
             _cache_result_req_queue = [];
             _cache_result_back_queue = {};
             cb(value);
