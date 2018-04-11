af5c628f44ca6fa53a5af0c920b5b32a40bfac03
linhey <158179948@qq.com>
2018-04-11 17:42:19 +0800

    update

--- storage.js
+++ storage.js
@@ -1,11 +1,11 @@
-var storage = function () {};
-
+var storage = function () {
+};
 
 var StorageEvent = {
 
     _listeners: {},
 
-    addEvent: function(type, fn) {
+    addEvent: function (type, fn) {
         if (typeof this._listeners[type] === "undefined") {
             this._listeners[type] = [];
         }
@@ -16,7 +16,7 @@ var StorageEvent = {
         return this;
     },
 
-    fireEvent: function(type, param) {
+    fireEvent: function (type, param) {
         var arrayEvent = this._listeners[type];
         if (arrayEvent instanceof Array) {
             for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
@@ -29,7 +29,7 @@ var StorageEvent = {
         return this;
     },
 
-    removeEvent: function(type, fn) {
+    removeEvent: function (type, fn) {
         var arrayEvent = this._listeners[type];
         if (typeof type === "string" && arrayEvent instanceof Array) {
             if (typeof fn === "function") {
@@ -48,8 +48,14 @@ var StorageEvent = {
     }
 };
 
-function cacheResult(name,value) {
-    StorageEvent.fireEvent(name,value)
+var cache_result_req_queue = [];
+var cache_result_back_queue = {};
+
+function cacheResult(name, value) {
+    cache_result_back_queue[name].push(value);
+    if (cache_result_back_queue.count() === cache_result_req_queue.count()) {
+        StorageEvent.fireEvent(name, cache_result_back_queue)
+    }
 }
 
 storage.prototype.app_cache = function (type, name, value) {
@@ -66,16 +72,22 @@ storage.prototype.app_cache = function (type, name, value) {
     } else {
         myWeb.postMessage('storage', JSON.stringify(body));
     }
-    ;
 };
 
 
 storage.prototype.get = function (list, cb) {
     if (native_config.wp < 2000) {
-        var name = list.pop();
-        this.app_cache('get',name);
-        StorageEvent.addEvent(name,function (value) {
+        cache_result_req_queue = list;
+        var name = JSON.stringify(list);
+
+        for (item in list) {
+            this.app_cache('get', item);
+        }
+
+        StorageEvent.addEvent(name, function (value) {
             StorageEvent.removeEvent(name);
+            cache_result_req_queue = {};
+            cache_result_back_queue = {};
             cb(value);
         });
         return;
@@ -92,7 +104,7 @@ storage.prototype.set = function (key, value, cb) {
     }
 
     var str = JSON.stringify(value);
-    str = '{"' + key + '":' + str +  '}';
+    str = '{"' + key + '":' + str + '}';
     var data = JSON.parse(str);
     Native.post('sp://cache/set', data, cb)
 };
