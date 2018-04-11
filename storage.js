15b5ddaffe3042c4dc119d7f866c9b56dd4ffbf6
linhey <158179948@qq.com>
2018-04-11 17:15:12 +0800

    update

--- storage.js
+++ storage.js
@@ -1,9 +1,57 @@
 var storage = function () {};
 
 
+var StorageEvent = {
+
+    _listeners: {},
+
+    addEvent: function(type, fn) {
+        if (typeof this._listeners[type] === "undefined") {
+            this._listeners[type] = [];
+        }
+        if (typeof fn === "function") {
+            this._listeners[type].push(fn);
+        }
+
+        return this;
+    },
+
+    fireEvent: function(type, param) {
+        var arrayEvent = this._listeners[type];
+        if (arrayEvent instanceof Array) {
+            for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
+                if (typeof arrayEvent[i] === "function") {
+                    arrayEvent[i](param);
+                }
+            }
+        }
+
+        return this;
+    },
+
+    removeEvent: function(type, fn) {
+        var arrayEvent = this._listeners[type];
+        if (typeof type === "string" && arrayEvent instanceof Array) {
+            if (typeof fn === "function") {
+                for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
+                    if (arrayEvent[i] === fn) {
+                        this._listeners[type].splice(i, 1);
+                        break;
+                    }
+                }
+            } else {
+                delete this._listeners[type];
+            }
+        }
+
+        return this;
+    }
+};
+
+storage.prototype.cbList = {};
+
 function cacheResult(name,value) {
-    alert(name)
-    alert(value)
+    StorageEvent.fireEvent(name,value)
 }
 
 storage.prototype.app_cache = function (type, name, value) {
@@ -26,7 +74,12 @@ storage.prototype.app_cache = function (type, name, value) {
 
 storage.prototype.get = function (list, cb) {
     if (native_config.wp < 2000) {
-        this.app_cache('get',list.pop());
+        var name = list.pop();
+        this.app_cache('get',name);
+        StorageEvent.addEvent(name,function (value) {
+            StorageEvent.removeEvent(name);
+            cb(value);
+        });
         return;
     }
     Native.post('sp://cache/get', {list: JSON.stringify(list)}, cb)
