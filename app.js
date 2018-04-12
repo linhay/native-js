e07966f8f09d5aa313e437d730589ab443ef1ccf
linhey <158179948@qq.com>
2018-04-12 15:02:09 +0800

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = function () {
 
 app.prototype.open = function (url) {
     if (native_config.wp < 2000) {
-        this.app_open({url: url});
+        Native.bridge_for_1('open', {url: url});
         return;
     }
     Native.post('sp://app/open', {url: url});
@@ -13,31 +13,54 @@ app.prototype.open = function (url) {
 
 app.prototype.location = function (cb) {
     if (native_config.wp < 2000) {
-        
+        _cache_result_req_queue = list;
+        var name = JSON.stringify(list);
+
+        var body = {
+            type: 'get',
+            name: 'location'
+        };
+        Native.bridge_for_1('storage', body);
+
+        NativeEvent.addEvent(name, function (value) {
+            NativeEvent.removeEvent(name);
+            _cache_result_req_queue = [];
+            _cache_result_back_queue = {};
+            cb(value);
+        });
         return;
     }
     Native.post('sp://app/location', cb);
 };
 
 app.prototype.share = function (params, cb) {
+    if (native_config.wp < 2000) {
+        Native.bridge_for_1('share', params);
+        return;
+    }
     Native.post('sp://app/share', params, cb);
 };
 
 app.prototype.pay = function (type, data) {
-    Native.post('sp://app/pay', data);
-}
-
-
-app.prototype.app_open = function (url) {
-    if (!(native_config.source === 0 || native_config.source === -1)) {
+    if (native_config.wp < 2000) {
+        var json = {};
+        if (type === 1 || type === 'wechat') {
+            var url = 'weixin://app/' + data.appid + '/pay/?';
+            url += 'nonceStr=' + data.noncestr;
+            url += '&package=' + encodeURIComponent(data.package);
+            url += '&partnerId=' + data.partnerid;
+            url += '&prepayId=' + data.prepayid;
+            url += '&timeStamp=' + data.timestamp;
+            url += '&sign=' + data.sign;
+            url += '&signType=SHA1';
+        Native.bridge_for_1('open',{url: url});
+        }
+        if (type === 2 || type === 'alipay') {
+            json.type = 2;
+            json.aliPay = dat.bill_sid;
+        }
+        Native.bridge_for_1('pay', json);
         return;
     }
-    var body = {
-        url: url
-    };
-    if (native_config.source === 0) {
-        window.webkit.messageHandlers.open.postMessage(body);
-    } else {
-        myWeb.postMessage('open', JSON.stringify(body));
-    }
+    Native.post('sp://app/pay', data);
 };
--- network.js
+++ network.js
@@ -1,7 +1,9 @@
 const network = {
 
     post: function (params, cb) {
+        if (native_config.wp < 2000) throw "不支持该版本";
         Native.post("sp://network/post", params, cb)
     }
 
-}
+
+};
