39d7b54a6b261d96f12f0a32fd26164e8b594ede
linhey <158179948@qq.com>
2018-04-12 11:39:22 +0800

    update

--- NativeHandle.js
+++ NativeHandle.js
@@ -3,7 +3,7 @@ var Native = {
 
     source: -1,
 
-    isHas: function(name) {
+    isHas: function (name) {
         try {
             return "function" === typeof eval(name);
         } catch (e) {
@@ -11,7 +11,7 @@ var Native = {
         }
     },
 
-    parserURL: function(urlObj) {
+    parserURL: function (urlObj) {
         const url = urlObj.toString();
         const a = document.createElement('a');
         a.href = url;
@@ -20,14 +20,14 @@ var Native = {
             host: a.hostname,
             query: a.search,
             path: a.pathname.replace(/^([^\/])/, '/$1'),
-            params: (function() {
+            params: (function () {
                 var ret = {};
-                var seg = a.search.replace(/^\?/, '').split('&').filter(function(v, i) {
+                var seg = a.search.replace(/^\?/, '').split('&').filter(function (v, i) {
                     if (v !== '' && v.indexOf('=')) {
                         return true;
                     }
                 });
-                seg.forEach(function(element, index) {
+                seg.forEach(function (element, index) {
                     var idx = element.indexOf('=');
                     var key = element.substring(0, idx);
                     var val = element.substring(idx + 1);
@@ -36,6 +36,18 @@ var Native = {
                 return ret;
             })()
         };
+    },
+
+    bridge_for_1: function (name, body) {
+        if (!(native_config.source === 0 || native_config.source === -1)) {
+            return
+        }
+        if (native_config.source === 0) {
+            var fun = eval('window.webkit.messageHandlers.' + name + '.postMessage');
+            fun(body);
+        } else {
+            myWeb.postMessage(name, JSON.stringify(body));
+        }
     },
 
     bridge: function (message) {
@@ -48,12 +60,12 @@ var Native = {
         }
     },
 
-    post: function(url, params, callBack) {
+    post: function (url, params, callBack) {
         Native.id += 1;
         var id = Native.id;
 
         // params默认值
-        if ("function" === typeof params  && !(callBack)) {
+        if ("function" === typeof params && !(callBack)) {
             callBack = params;
             params = {}
         }
@@ -85,17 +97,17 @@ var Native = {
             this.bridge(message);
         } else {
             if (!NativeEvent._listeners[id]) {
-                NativeEvent.addEvent(id, function(data) {
+                NativeEvent.addEvent(id, function (data) {
                     NativeEvent.removeEvent(id);
                     callBack(data);
                 });
             }
-        this.bridge(message);
+            this.bridge(message);
         }
 
     },
 
-    callBack: function(callBackID, data) {
+    callBack: function (callBackID, data) {
         if ('object' === typeof data) {
             NativeEvent.fireEvent(callBackID, data);
         } else {
@@ -103,7 +115,7 @@ var Native = {
         }
     },
 
-    removeAllCallBacks: function(data) {
+    removeAllCallBacks: function (data) {
         NativeEvent._listeners = {};
     }
 
@@ -113,7 +125,7 @@ var NativeEvent = {
 
     _listeners: {},
 
-    addEvent: function(type, fn) {
+    addEvent: function (type, fn) {
         if (typeof this._listeners[type] === "undefined") {
             this._listeners[type] = [];
         }
@@ -123,7 +135,7 @@ var NativeEvent = {
         return this;
     },
 
-    fireEvent: function(type, param) {
+    fireEvent: function (type, param) {
         var arrayEvent = this._listeners[type];
         if (arrayEvent instanceof Array) {
             for (var i = 0, length = arrayEvent.length; i < length; i += 1) {
@@ -135,7 +147,7 @@ var NativeEvent = {
         return this;
     },
 
-    removeEvent: function(type, fn) {
+    removeEvent: function (type, fn) {
         var arrayEvent = this._listeners[type];
         if (typeof type === "string" && arrayEvent instanceof Array) {
             if (typeof fn === "function") {
@@ -162,7 +174,7 @@ var NativeEvent = {
 *  References:
 *    http://en.wikipedia.org/wiki/Base64
 */
-;(function(global, factory) {
+;(function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined'
         ? module.exports = factory(global)
         : typeof define === 'function' && define.amd
@@ -172,7 +184,7 @@ var NativeEvent = {
         : typeof window !== 'undefined' ? window
         : typeof global !== 'undefined' ? global
             : this
-), function(global, Base64) {
+), function (global, Base64) {
     'use strict';
     // existing version for noConflict()
     var _Base64 = global.Base64;
@@ -188,14 +200,14 @@ var NativeEvent = {
     // constants
     var b64chars
         = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-    var b64tab = function(bin) {
+    var b64tab = function (bin) {
         var t = {};
         for (var i = 0, l = bin.length; i < l; i++) t[bin.charAt(i)] = i;
         return t;
     }(b64chars);
     var fromCharCode = String.fromCharCode;
     // encoder stuff
-    var cb_utob = function(c) {
+    var cb_utob = function (c) {
         if (c.length < 2) {
             var cc = c.charCodeAt(0);
             return cc < 0x80 ? c
@@ -215,10 +227,10 @@ var NativeEvent = {
         }
     };
     var re_utob = /[\uD800-\uDBFF][\uDC00-\uDFFFF]|[^\x00-\x7F]/g;
-    var utob = function(u) {
+    var utob = function (u) {
         return u.replace(re_utob, cb_utob);
     };
-    var cb_encode = function(ccc) {
+    var cb_encode = function (ccc) {
         var padlen = [0, 2, 1][ccc.length % 3],
             ord = ccc.charCodeAt(0) << 16
                 | ((ccc.length > 1 ? ccc.charCodeAt(1) : 0) << 8)
@@ -231,32 +243,32 @@ var NativeEvent = {
             ];
         return chars.join('');
     };
-    var btoa = global.btoa ? function(b) {
+    var btoa = global.btoa ? function (b) {
         return global.btoa(b);
-    } : function(b) {
+    } : function (b) {
         return b.replace(/[\s\S]{1,3}/g, cb_encode);
     };
     var _encode = buffer ?
-        buffer.from && buffer.from !== Uint8Array.from ? function(u) {
+        buffer.from && buffer.from !== Uint8Array.from ? function (u) {
                 return (u.constructor === buffer.constructor ? u : buffer.from(u))
                     .toString('base64')
             }
-            : function(u) {
+            : function (u) {
                 return (u.constructor === buffer.constructor ? u : new buffer(u))
                     .toString('base64')
             }
-        : function(u) {
+        : function (u) {
             return btoa(utob(u))
         }
     ;
-    var encode = function(u, urisafe) {
+    var encode = function (u, urisafe) {
         return !urisafe
             ? _encode(String(u))
-            : _encode(String(u)).replace(/[+\/]/g, function(m0) {
+            : _encode(String(u)).replace(/[+\/]/g, function (m0) {
                 return m0 == '+' ? '-' : '_';
             }).replace(/=/g, '');
     };
-    var encodeURI = function(u) {
+    var encodeURI = function (u) {
         return encode(u, true)
     };
     // decoder stuff
@@ -265,7 +277,7 @@ var NativeEvent = {
         '[\xE0-\xEF][\x80-\xBF]{2}',
         '[\xF0-\xF7][\x80-\xBF]{3}'
     ].join('|'), 'g');
-    var cb_btou = function(cccc) {
+    var cb_btou = function (cccc) {
         switch (cccc.length) {
             case 4:
                 var cp = ((0x07 & cccc.charCodeAt(0)) << 18)
@@ -288,10 +300,10 @@ var NativeEvent = {
                 );
         }
     };
-    var btou = function(b) {
+    var btou = function (b) {
         return b.replace(re_btou, cb_btou);
     };
-    var cb_decode = function(cccc) {
+    var cb_decode = function (cccc) {
         var len = cccc.length,
             padlen = len % 4,
             n = (len > 0 ? b64tab[cccc.charAt(0)] << 18 : 0)
@@ -306,32 +318,32 @@ var NativeEvent = {
         chars.length -= [0, 0, 2, 1][padlen];
         return chars.join('');
     };
-    var atob = global.atob ? function(a) {
+    var atob = global.atob ? function (a) {
         return global.atob(a);
-    } : function(a) {
+    } : function (a) {
         return a.replace(/[\s\S]{1,4}/g, cb_decode);
     };
     var _decode = buffer ?
-        buffer.from && buffer.from !== Uint8Array.from ? function(a) {
+        buffer.from && buffer.from !== Uint8Array.from ? function (a) {
                 return (a.constructor === buffer.constructor
                     ? a : buffer.from(a, 'base64')).toString();
             }
-            : function(a) {
+            : function (a) {
                 return (a.constructor === buffer.constructor
                     ? a : new buffer(a, 'base64')).toString();
             }
-        : function(a) {
+        : function (a) {
             return btou(atob(a))
         };
-    var decode = function(a) {
+    var decode = function (a) {
         return _decode(
-            String(a).replace(/[-_]/g, function(m0) {
+            String(a).replace(/[-_]/g, function (m0) {
                 return m0 == '-' ? '+' : '/'
             })
                 .replace(/[^A-Za-z0-9\+\/]/g, '')
         );
     };
-    var noConflict = function() {
+    var noConflict = function () {
         var Base64 = global.Base64;
         global.Base64 = _Base64;
         return Base64;
@@ -352,20 +364,20 @@ var NativeEvent = {
     };
     // if ES5 is available, make Base64.extendString() available
     if (typeof Object.defineProperty === 'function') {
-        var noEnum = function(v) {
+        var noEnum = function (v) {
             return {value: v, enumerable: false, writable: true, configurable: true};
         };
-        global.Base64.extendString = function() {
+        global.Base64.extendString = function () {
             Object.defineProperty(
-                String.prototype, 'fromBase64', noEnum(function() {
+                String.prototype, 'fromBase64', noEnum(function () {
                     return decode(this)
                 }));
             Object.defineProperty(
-                String.prototype, 'toBase64', noEnum(function(urisafe) {
+                String.prototype, 'toBase64', noEnum(function (urisafe) {
                     return encode(this, urisafe)
                 }));
             Object.defineProperty(
-                String.prototype, 'toBase64URI', noEnum(function() {
+                String.prototype, 'toBase64URI', noEnum(function () {
                     return encode(this, true)
                 }));
         };
@@ -383,7 +395,7 @@ var NativeEvent = {
     }
     else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define([], function() {
+        define([], function () {
             return global.Base64
         });
     }
--- navigatior.js
+++ navigatior.js
@@ -1,36 +1,5 @@
 var nav = function () {};
 
-nav.prototype.app_execute = function(body) {
-    if (!(native_config.source === 0 || native_config.source === -1)) { return }
-    if (native_config.source === 0) {
-     var fun = eval('window.webkit.messageHandlers.execute.postMessage');
-     fun(body);
-        // window.webkit.messageHandlers.execute.postMessage(body);
-    } else {
-        myWeb.postMessage('execute', JSON.stringify(body));
-    }
-};
-
-nav.prototype.app_pop = function(name) {
-    if (!(native_config.source === 0 || native_config.source === -1)) { return }
-    var data = {'vcName': name};
-    if (native_config.source === 0) {
-        window.webkit.messageHandlers.pop.postMessage(data);
-    } else {
-        myWeb.postMessage('pop', JSON.stringify(data));
-    }
-};
-
-nav.prototype.app_push = function(body) {
-    if (!(native_config.source === 0 || native_config.source === -1)) { return }
-    if (native_config.source === 0) {
-        window.webkit.messageHandlers.push.postMessage(body);
-    } else {
-        myWeb.postMessage('push', JSON.stringify(body));
-    }
-};
-
-
 /* 回退至指定app页面
 *  value: 可缺省 | 指定页面名称 | 回退层数
 *
@@ -46,36 +15,43 @@ nav.prototype.pop = function (value) {
         if (native_config.source === 0) {
             var data = {};
             data.url = 'sp://tabbar/selectIndex?index=' + index;
-            this.app_execute(data);
+            Native.bridge_for_1('execute', data);
             data.vcName = 'tabbar';
-            this.app_pop(data)
+            Native.bridge_for_1('pop', data);
         } else {
             data = {};
-            data.url = "sp://" + value + "/" + value
-            this.app_push(data)
+            data.url = "sp://" + value + "/" + value;
+            Native.bridge_for_1('push', data);
         }
     }
 
     // 1.x 兼容代码
-    if (native_config.wp < 2000){
+    if (native_config.wp < 2000) {
         switch (value) {
-            case 'home': special(0);
-            case 'cateory': special(1);
-            case 'message': special(2);
-            case 'shopcart': special(3);
-            case 'myProfile': special(4);
-            default: this.app_pop(value);
+            case 'home':
+                special(0);
+            case 'cateory':
+                special(1);
+            case 'message':
+                special(2);
+            case 'shopcart':
+                special(3);
+            case 'myProfile':
+                special(4);
+            default:
+                Native.bridge_for_1('pop', {vcName: value});
         }
         return
     }
 
     // 2.x 代码
     const params = {};
-    if (value === undefined || value === "" || value === null) {
-    } else if (/^(-)?\d+(\.\d+)?$/.exec(value) == null) {
-        params.name = value
-    } else {
-        params.index = value
+    if (!(value)) {
+        if (null == /^(-)?\d+(\.\d+)?$/.exec(value)) {
+            params.name = value
+        } else {
+            params.index = value
+        }
     }
     Native.post('sp://navigatior/pop', params)
 };
@@ -101,20 +77,20 @@ nav.prototype.backList = function (cb) {
 *       show('sp://home/home',{isRemoveSelf: 0})
 *       show('sp://home/home')
 * */
-nav.prototype.show = function (url,params) {
+nav.prototype.show = function (url, params) {
 
     var data = {};
     if (params) {
         params.url = url;
         data = params;
-    }else{
+    } else {
         data.url = url;
     }
 
     if (native_config.wp < 2000) {
         // 1.x 代码
-        this.app_push(data);
-    }else{
+        Native.bridge_for_1('push', data)
+    } else {
         // 2.x 代码
         Native.post('sp://navigatior/show', data)
     }
